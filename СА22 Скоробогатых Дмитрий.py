8d5e55b7859e075d398b7c444bf3438c7a6842aa
SkorobogatyhDm <149555912+SkorobogatyhDm@users.noreply.github.com>
2024-09-06 16:36:42 +0300

    Add files via upload

---
+++ СА22 Скоробогатых Дмитрий.py
@@ -0,0 +1,61 @@
+#import math
+#def tasks18(a, b, i, N):
+    #a = inp1 * 8 
+    #b = inp2 * inp3 * inp4
+    #i = a / b
+    #N = (2 ** i)
+    #return int (N)
+#if __name__=="__main__":
+    #inp1 = float(input("Введите сколько байт "))
+    #inp2 = float(input("Введите сколько страниц "))
+    #inp3 = float(input("Введите сколько строк "))
+    #inp4 = float(input("Введите сколько символов "))
+    #print(tasks18(inp1, inp2, inp3, inp4))
+
+    #import math
+#def tasks19(a, b):
+    #a = 64 
+    #b = 32
+    #i = (math.log2(a)) / (math.log2(b))
+    #return (i)
+#if __name__=="__main__":
+    #inp1 = float(input("Введите данные первого алфавита "))
+    #inp2 = float(input("Введите данные второго алфавита "))
+    #print(tasks19(inp1, inp2))
+
+    #import math
+#def tasks20(a, N,):
+    #a = 120 
+    #N = 64
+    #i = (a / (math.log2(N)))
+    #return int (i)
+#if __name__=="__main__":
+    #inp1 = float(input("Введите данные алфавита "))
+    #inp2 = float(input("сколько бит "))
+    #print(tasks20(inp1, inp2))
+
+    #import math
+#def tasks19(a):
+    #i = (2 ** a)
+    #return int (i)
+#if __name__=="__main__":
+    #inp1 = float(input("Введите бит "))
+    #print(tasks19(inp1))
+
+    #import math
+#def tasks20(a):
+    #i = (2 ** a)
+    #return int (i)
+#if __name__=="__main__":
+    #inp1 = float(input("Введите бит "))
+    #print(tasks20(inp1))
+
+    #def task7(Wk, Bk, c):
+    #Kk = (Wk * Bk) / (2 ** c)
+    #return int(Kk)
+
+#if __name__ == "__main__":
+    #inp1 = int(input("Количество белой краски: "))
+    #inp2 = int(input("Количество синей краски: "))
+    #inp3 = int(input("Вес одной белой банки: "))
+    #print(task7(inp1, inp2, inp3))
